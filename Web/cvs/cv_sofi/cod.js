97693841ef4ac8134727511605bcfcccfbdf4941
sofiaparoba <a340393067@my.uvm.edu.mx>
2024-02-20 16:45:31 -0600

    Cambios cv
    Agregar directorios para cv

---
+++ Web/cvs/cv_sofi/cod.js
@@ -0,0 +1,15 @@
+//Comentario
+function saludo(){
+    alert("hola JS")
+}
+
+function numeracion(tope){
+    for(i=1;i<=tope;i++){
+        alert(i);
+    }
+}
+
+function fondo(){
+    alert("click")
+    parrafos=document.getElementsByTagName("p")
+}
